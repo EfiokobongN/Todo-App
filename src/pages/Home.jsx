19fd8b1c268baf009eee638774ebb46bd7d0ecb7
EfiokobongN <mailpreciousnelson@gmail.com>
2023-11-10 12:32:47 +0000

    commit raect

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Chat from '../components/chat/Chat'
+
+function Home() {
+  return (
+    <div>
+        <Chat />
+      Welcome to Home Page
+    </div>
+  )
+}
+
+export default Home
