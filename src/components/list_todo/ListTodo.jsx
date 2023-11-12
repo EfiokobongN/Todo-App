7bdba422ea31dbcc1f06e4f15b744f59d0dd1b4b
EfiokobongN <mailpreciousnelson@gmail.com>
2023-11-12 17:31:41 +0000

    item todo complete

--- src/components/list_todo/ListTodo.jsx
+++ src/components/list_todo/ListTodo.jsx
@@ -8,6 +8,26 @@ function ListTodo() {
         <div className="listTodo_item">Buy New Laptop</div>
         <div className="listTodo_delete">Del</div>
       </div>
+
+      <div className="listTodo_row">
+        <div className="listTodo_item">Make Codehence Course</div>
+        <div className="listTodo_delete">Del</div>
+      </div>
+
+      <div className="listTodo_row">
+        <div className="listTodo_item">Complete Today's Task</div>
+        <div className="listTodo_delete">Del</div>
+      </div>
+
+      <div className="listTodo_row">
+        <div className="listTodo_item">Create Profolio Site</div>
+        <div className="listTodo_delete">Del</div>
+      </div>
+
+      <div className="listTodo_row">
+        <div className="listTodo_item">Reviews Assignment</div>
+        <div className="listTodo_delete">Del</div>
+      </div>
     </div>
   )
 }
